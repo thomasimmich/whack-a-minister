2e1d06e9c26cfa3c4bf7f2a04fa384632cab8dab
thomas.immich <thomas.immich@centigrade.de>
2023-04-22 17:28:29 +0200

    Refactoring

--- src/components/three/Enemy.tsx
+++ src/components/three/Enemy.tsx
@@ -1,19 +1,23 @@
 import { Box } from '@react-three/drei';
+import { useLoader } from '@react-three/fiber';
+import { TextureLoader } from 'three';
+import { useWindowSize } from '../../hooks/useWindowSize';
 
 export interface EnemyProps {
   index: number;
 }
 
 export function Enemy(props: EnemyProps) {
-  //const textureURL = 'src/assets/images/train/train-' + props.type + '.png';
-  //const texture = useLoader(TextureLoader, textureURL);
-  const faceWidth = 0.1;
-  const faceHeight = 0.1;
+  const textureURL = 'src/assets/images/people/enemy-a.png';
+  const texture = useLoader(TextureLoader, textureURL);
+  const windowSize = useWindowSize();
+  const faceWidth = texture.image.width / windowSize.width / 2;
+  const faceHeight = texture.image.height / windowSize.width / 2;
 
   // Return the view, these are regular Threejs elements expressed in JSX
   return (
     <Box position={[props.index * faceWidth, 0, 0.1]} args={[faceWidth, faceHeight, 1]}>
-      <meshBasicMaterial color="blue" />
+      <meshBasicMaterial map={texture} transparent />
     </Box>
   );
 }
--- src/components/three/Train.tsx
+++ src/components/three/Train.tsx
@@ -1,35 +1,18 @@
-import { useFrame, useLoader } from '@react-three/fiber';
-import { useState } from 'react';
-import { TextureLoader } from 'three';
-import { useWindowSize } from '../../hooks/useWindowSize';
+import { useTrainPosition } from '../../hooks/useTrainPosition';
 import { TrainCoach } from './TrainCoach';
 export interface TrainProps {
-  coachCode?: string;
-
-  timeliness?: number;
+  coachCode: string;
 }
 export function Train(
   props: TrainProps = {
     coachCode: '',
-    timeliness: 1,
   },
 ) {
-  const [currentTimeliness, setCurrentTimeliness] = useState(1);
-
-  const textureURL = 'src/assets/images/train/train-a.png';
-  const texture = useLoader(TextureLoader, textureURL);
-  const windowSize = useWindowSize();
-  const coachWidth = texture.image.width / windowSize.width;
-  const trainPosition = -(coachWidth * props.coachCode!.length) / 2;
-
   const coachComponents = props.coachCode
     ?.split('')
     .map((char, index) => <TrainCoach key={index} index={index} type={char} />);
+  const trainPosition = useTrainPosition(props.coachCode);
 
-  useFrame((_state, delta) => {
-    const newTimeliness = currentTimeliness! - delta * 0.05;
-    setCurrentTimeliness(newTimeliness);
-  });
   // Return the view, these are regular Threejs elements expressed in JSX
-  return <group position={[trainPosition - currentTimeliness!, 0, 0]}>{coachComponents}</group>;
+  return <group position={[trainPosition, 0, 0]}>{coachComponents}</group>;
 }
