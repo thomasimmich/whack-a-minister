dc19d125990f6704717825599b31296f06d897b4
ValoriaStudios <heyjohn655@gmail.com>
2023-04-23 16:20:39 +0200

    menu

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 
 import { useContext, useEffect, useRef, useState } from 'react';
 import { Entity } from 'tick-knock';
+import { IoHome, IoStatsChart, IoCaretForward } from "react-icons/io5"
 
 import { ECS, ECSContext } from './app/ECSContext';
 import {
@@ -17,7 +18,6 @@ import { FullScreenCanvas } from './components/three/FullScreenCanvas';
 
 import { useAnimationFrame } from 'framer-motion';
 import { System } from 'tick-knock';
-import { BASE_ASSET_URL } from './base/Constants';
 import { Scores } from './components/three/Score';
 import { TrainWithPeople } from './components/three/TrainWithPeople';
 import { useWindowSize } from './hooks/useWindowSize';
@@ -25,8 +25,11 @@ import { HighscoreLoadingSystem } from './systems/HighscoreLoadingSystem';
 import { ScoreEvaluationSystem } from './systems/ScoreEvaluationSystem';
 
 import { StaticBoxContainer } from './components/three/StaticBoxContainer';
-import { useRenderSystemEntities } from './hooks/useRenderSystemEntities';
-import Menu from './pages/Menu';
+//import { useRenderSystemEntities } from './hooks/useRenderSystemEntities';
+import { BASE_ASSET_URL } from './base/Constants';
+
+
+
 
 const TriggerRenderAppSystems = () => {
   const ecs = useContext(ECSContext);
@@ -99,12 +102,20 @@ function App() {
 
   const mainTuneSoundRef = useRef<any>(null);
 
-  const [gameStates] = useRenderSystemEntities((e) => e.has(GameStateFacet));
+  //const [gameStates] = useRenderSystemEntities((e) => e.has(GameStateFacet));
+  const [activeScore, setActiveScore] = useState(false)
+  const ScoreIlustration = BASE_ASSET_URL + '/images/menu/ScoreView.png'
+  const Card =  BASE_ASSET_URL + '/images/menu/card-1.png';
 
-  const [isWelcomeVisible, setWelcomeVisible] = useState(true);
+
+  function toggleActiveScore() {setActiveScore(!activeScore)}
+  function play() {setPlayingVisible(true)}
+
+
+
   const [isPlayingVisible, setPlayingVisible] = useState(false);
-  const [isHighscoreVisible, setHighscoreVisible] = useState(false);
 
+{/*
   useEffect(() => {
     const gameStateEntity = gameStates[0];
     const gameState = gameStateEntity?.get(GameStateFacet)?.props.gameState;
@@ -121,13 +132,67 @@ function App() {
       setPlayingVisible(false);
       setHighscoreVisible(true);
     }
-  }, [gameStates]);
+  }, [gameStates]); */}
 
   return (
     <div className="w-screen m-0 p-0 h-screen ">
         <ECSContext.Provider value={ecs}>
-      {isWelcomeVisible ? (<>
-        <Menu />
+      {!isPlayingVisible ? (<>
+        <>
+      {!activeScore ? (
+        /* Home Ansicht */
+        <div  className='w-full h-full p-8   text-black'>
+        <div className='flex justify-between w-full'>
+          <div>
+            <p className=' text-On-Surface-Variant '>SAMSTAG, 11. FEBRUAR</p>
+            <p className='text-3xl font-bold'>Home</p>
+          </div>
+          <div className=' flex text-3xl  text-system-blue'>
+            <div>
+              <IoHome className='mr-3 w-full justify-center' />
+              <p className=' text-sm w-full '>Home</p>
+            </div>
+            <div onClick={toggleActiveScore} className=' ml-2'>
+              <IoStatsChart  className='mr-3  text-icon w-full justify-center' />
+              <p className=' text-sm w-full text-icon '>Score</p>
+            </div>
+          </div>
+        </div>
+  
+        <div className='mt-6'>
+          <div onClick={play} className='w-full h-40 rounded-xl p-5 flex items-end'  style={{ backgroundImage: `url(${Card})`,}}>
+            <p className='text-3xl font-bold text-white flex  '>Play <IoCaretForward className="text-3xl mt-1 ml-1"/></p>
+          </div>
+        </div>
+      </div>
+      ) : (<>
+        {/* Score Ansicht */}
+        <div  className='w-full h-full  text-black'>
+          <div className='flex p-8 h-2/4 justify-between w-full'>
+            <div>
+              <p className=' text-On-Surface-Variant '>SAMSTAG, 11. FEBRUAR</p>
+              <p className='text-3xl font-bold'>Score</p>
+            </div>
+            <div className=' flex text-3xl  text-system-blue'>
+              <div  onClick={toggleActiveScore} >
+                <IoHome className='mr-3 w-full text-icon justify-center' />
+                <p className=' text-sm w-full  text-icon  '>Home</p>
+              </div>
+              <div className=' ml-2'>
+                <IoStatsChart className='mr-3   w-full justify-center' />
+                <p className=' text-sm w-full'>Score</p>
+              </div>
+           
+            </div>
+          </div>
+
+        
+          <div className='h-2/4  w-full justify-end flex items-end'>
+            <img className='w-96 ' src={ScoreIlustration} />
+          </div>
+        </div>
+      </>)}
+    </>
       </>) : isPlayingVisible ? (
         <>
         
@@ -163,8 +228,6 @@ function App() {
             {/* <UpdateOnRenderAppSystems /> */}
 
         </>
-      ) : isHighscoreVisible ? (
-        <></> 
       ) : (
         <>.</>
       )}
