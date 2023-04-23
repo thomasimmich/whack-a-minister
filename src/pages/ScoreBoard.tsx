42cb19948968617ba3344a1e5b4e364d8b13ee98
ulrike.kulzer <ulrike.kulzer@centigrade.de>
2023-04-23 13:12:01 +0200

    style-fix: fixed score image path

--- src/pages/ScoreBoard.tsx
+++ src/pages/ScoreBoard.tsx
@@ -1,8 +1,13 @@
+import { BASE_ASSET_URL } from '../base/Constants';
+
 const ScoreBoard = () => {
+  const ScoreIlustration = BASE_ASSET_URL + '/images/menu/ScoreView.png';
   return (
     <>
       <h1>Scoreboard</h1>
-      <div className="h-2/4  flex items-end">{/* <img src={ScoreIlustration} /> */}</div>
+      <div className="h-2/4  flex items-end">
+        <img src={ScoreIlustration} />
+      </div>
     </>
   );
 };
