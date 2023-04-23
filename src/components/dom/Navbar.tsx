c09f068378e3794fe2dbc1a60fd554484cc228c4
ulrike.kulzer <ulrike.kulzer@centigrade.de>
2023-04-23 13:29:03 +0200

    feature: display current date and week day

--- src/components/dom/Navbar.tsx
+++ src/components/dom/Navbar.tsx
@@ -2,10 +2,19 @@ import { IoHome, IoStatsChart } from 'react-icons/io5';
 
 // TODO: fix nav bar (score -> score, home -> home => no toggle!)
 const Navbar = ({ togglePage }) => {
+  const currentDate = () => {
+    const currentWeekDay = new Date().toLocaleDateString(window.navigator.language, {
+      weekday: 'long',
+    });
+    const currentDate = new Date().toLocaleDateString();
+    const readableDate = currentWeekDay + ', ' + currentDate;
+    return readableDate;
+  };
+
   return (
     <div className="flex justify-between w-full">
       <div>
-        <p className=" text-On-Surface-Variant ">SAMSTAG, 11. FEBRUAR</p>
+        <p className=" text-On-Surface-Variant ">{currentDate()}</p>
       </div>
       <div className=" flex text-3xl  text-system-blue">
         <div onClick={() => togglePage()}>
